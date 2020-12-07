7849fb792f06007a53aabb79dc9d8317285148ae
snarkusius <marcus@lengquist.se>
2020-12-07 14:29:23 +0100

    firstcommit

--- helpers.js
+++ helpers.js
@@ -15,3 +15,14 @@ export function randomArray(len, n) {
 function randomInt(n) {
     return Math.floor(Math.random() * (n + 1))
 }
+
+export function swap(arr,num1,num2) {
+    let temporarystorige = arr[num1]
+    arr[num1] = arr[num2]
+    arr[num2] = temporarystorige
+}
+const a = [5, 8, 10, 9]
+
+swap(a, 1, 3) // byter plats på a[1] och a[3]
+
+console.log(a) // [5, 9, 10, 8]
--- sorting.js
+++ sorting.js
@@ -1,3 +1,17 @@
-import { randomArray } from './helpers.js'
-
+import { randomArray} from './helpers.js'
+import { swap} from './helpers.js'
 const a = randomArray(100, 10)
+
+function bubelsort(arr) {
+    let newarr = arr
+    for (let i = 0; i < newarr.length; i++) {
+        for (let i2 = 0; i2 < newarr.length - i; i2++) {
+            if (newarr[i2] > newarr[(i2+1)]){
+                swap(newarr,i2,(i2+1))
+            }
+            
+        }
+    }
+    return newarr
+}
+console.log (bubelsort([9,8,7,690,4,3,2,1]))
